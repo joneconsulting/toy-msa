2cab69b27a4f9256289e694ca600a945f5096f0f
edowon0623@gmail.com <Dwlee$09>
2022-11-16 09:13:31 +0900

    updated the newest sources

---
+++ user-service/src/main/java/com/example/userservice/client/CatalogServiceClient.java
@@ -0,0 +1,18 @@
+package com.example.userservice.client;
+
+import com.example.userservice.error.FeignErrorDecoder;
+import com.example.userservice.vo.ResponseOrder;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name="order-service", configuration = FeignErrorDecoder.class)
+public interface OrderServiceClient {
+
+//    @GetMapping("/order-service/{userId}/orders_wrong_url")
+    @GetMapping("/order-service/{userId}/orders")
+    List<ResponseOrder> getOrders(@PathVariable String userId);
+
+}
