af3d2e63acd37088d50e35ce1a6ffae962905d86
edowon0623@gmail.com <Dwlee$09>
2023-05-10 08:30:26 +0900

    added a kafka test code

--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -1,29 +1,34 @@
 package com.example.orderservice.messagequeue;
 
-//import org.apache.kafka.clients.producer.ProducerConfig;
-//import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 
-//import org.springframework.kafka.annotation.EnableKafka;
-//import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-//import org.springframework.kafka.core.KafkaTemplate;
-//import org.springframework.kafka.core.ProducerFactory;
+import java.util.HashMap;
+import java.util.Map;
 
-//@EnableKafka
+@EnableKafka
 @Configuration
 public class KafkaProducerConfig {
-//    @Bean
-//    public ProducerFactory<String, String> producerFactory() {
-//        Map<String, Object> properties = new HashMap<>();
-//        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.192.188:9092");
-//        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//
-//        return new DefaultKafkaProducerFactory<>(properties);
-//    }
-//
-//    @Bean
-//    public KafkaTemplate<String, String> kafkaTemplate() {
-//        return new KafkaTemplate<>(producerFactory());
-//    }
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> properties = new HashMap<>();
+//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "[aws_ec2_ip_address]:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(properties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 }
--- order-service/src/main/java/com/example/orderservice/messagequeue/OrderProducer.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/OrderProducer.java
@@ -4,17 +4,17 @@ import com.example.orderservice.dto.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
-//import org.springframework.kafka.core.KafkaTemplate;
-
 @Service
 @Slf4j
 public class OrderProducer {
-//    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     List<Field> fields = Arrays.asList(new Field("string", true, "order_id"),
             new Field("string", true, "user_id"),
@@ -29,10 +29,10 @@ public class OrderProducer {
             .name("orders")
             .build();
 
-//    @Autowired
-//    public OrderProducer(KafkaTemplate<String, String> kafkaTemplate) {
-//        this.kafkaTemplate = kafkaTemplate;
-//    }
+    @Autowired
+    public OrderProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
 
     public OrderDto send(String topic, OrderDto orderDto) {
         Payload payload = Payload.builder()
@@ -54,7 +54,7 @@ public class OrderProducer {
             ex.printStackTrace();
         }
 
-//        kafkaTemplate.send(topic, jsonInString);
+        kafkaTemplate.send(topic, jsonInString);
         log.info("Order Producer sent data from the Order microservice: " + kafkaOrderDto);
 
         return orderDto;
