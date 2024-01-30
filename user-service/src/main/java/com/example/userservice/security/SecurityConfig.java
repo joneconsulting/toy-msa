ebb8299f0b4772639d7bb74d76e1d2c2e8a41998
edowon0623@gmail.com <Dwlee$09>
2024-01-30 23:29:08 +0900

    modifying the user-service (not finished)

--- user-service/src/main/java/com/example/userservice/security/SecurityConfig.java
+++ user-service/src/main/java/com/example/userservice/security/SecurityConfig.java
@@ -1,10 +1,32 @@
 package com.example.userservice.security;
 
+import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.util.function.Supplier;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -12,55 +34,75 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @ConditionalOnDefaultWebSecurity
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class SecurityConfig {
-//    private UserService userService;
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private Environment env;
-//
-//    public SecurityConfig(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-//        this.env = env;
-//        this.userService = userService;
-//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-//    }
-//
-//    @Bean
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER)
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/users/**").permitAll();
-//        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
-//        http.authorizeRequests().antMatchers("/health_check/**").permitAll();
-//        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
-//        http.authorizeRequests().antMatchers("/error/**").permitAll();
-//        http.authorizeRequests().antMatchers("/**")
-////                .hasIpAddress(env.getProperty("gateway.ip")) // <- IP 변경
-////                .hasIpAddress("127.0.0.1") // <- IP 변경
-//                .access("hasIpAddress('127.0.0.1')")
-//                .and()
-//                .addFilter(getAuthenticationFilter());
-//
-////        http.authorizeRequests().antMatchers("/users")
-////                .hasIpAddress(env.getProperty("gateway.ip")) // <- IP 변경
-////                .and()
-////                .addFilter(getAuthenticationFilter());
-////
-////        http.authorizeRequests().anyRequest().denyAll();
-//
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public static final String ALLOWED_IP_ADDRESS = "127.0.0.1";
+//    public static final String SUBNET = "/32";
+    public static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS);
+
+
+    public SecurityConfig(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/ignore1", "/ignore2");
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+//                                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+//                                .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+//                                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/actuator/**").permitAll()
+                                .requestMatchers("/users/**").permitAll()
+                                .requestMatchers("/health_check/**").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .requestMatchers("/error/**").permitAll()
+                                .requestMatchers("/**").access(this::hasIpAddress)
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilter(getAuthenticationFilter());
+
+        return http.build();
+
 //        http.headers().frameOptions().disable();
-//
-//        return http.build();
-//    }
-//
-//    private com.example.userservice.security.AuthenticationFilter getAuthenticationFilter() throws Exception {
-//        com.example.userservice.security.AuthenticationFilter authenticationFilter =
-//                new com.example.userservice.security.AuthenticationFilter(authenticationManager(), userService, env);
-//
-//        return authenticationFilter;
-//    }
-//
-//    // select pwd from users where email=?
-//    // db_pwd(encrypted) == input_pwd(encrypted)
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-//    }
+    }
+
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        return new AuthorizationDecision(ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()));
+    }
+
+    private com.example.userservice.security.AuthenticationFilter getAuthenticationFilter() throws Exception {
+        com.example.userservice.security.AuthenticationFilter authenticationFilter =
+                new com.example.userservice.security.AuthenticationFilter(authenticationManager(), userService, env);
+
+        return authenticationFilter;
+    }
+
+    // select pwd from users where email=?
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
+                                                       UserDetailsService userDetailsService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
 }
