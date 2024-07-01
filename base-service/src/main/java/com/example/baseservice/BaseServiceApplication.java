69d1a372afbc6fbeba343deb813c5245ad4916c7
edowon0623@gmail.com <Dwlee$09>
2024-07-01 13:19:53 +0900

    added a base service app for eureka test.

---
+++ base-service/src/main/java/com/example/baseservice/BaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.baseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseServiceApplication.class, args);
+    }
+
+}
---
+++ base-service/src/test/java/com/example/baseservice/BaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
