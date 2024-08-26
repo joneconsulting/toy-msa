f06890dd8c7ec6298226325586b5c30447c9e249
edowon0623@gmail.com <Dwlee$09>
2024-08-27 00:08:56 +0900

    remove a white space

--- apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -47,7 +47,7 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
             }
 
             String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-            String jwt = authorizationHeader.replace("Bearer", "");
+            String jwt = authorizationHeader.replace("Bearer ", "");
 
             if (!isJwtValid(jwt)) {
                 return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
