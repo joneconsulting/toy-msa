529e301d2bf4fcf7da69670997921e69af1b27e4
Dowon Lee <edowon0623@gmail.com>
2025-03-18 14:54:39 +0900

    docker-compose-kafka 파일 추가

--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -57,9 +57,9 @@ public class OrderController {
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
         /* jpa */
-//        OrderDto createdOrder = orderService.createOrder(orderDto);
-//        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
-        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+        OrderDto createdOrder = orderService.createOrder(orderDto);
+        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
 
         /* kafka */
         orderDto.setOrderId(UUID.randomUUID().toString());
@@ -67,7 +67,7 @@ public class OrderController {
 
         /* send this order to the kafka */
         kafkaProducer.send("example-catalog-topic", orderDto);
-        orderProducer.send("example-order-topic", orderDto);
+//        orderProducer.send("example-order-topic", orderDto);
 
         log.debug("After added orders data");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
--- order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
+++ order-service/src/main/java/com/example/orderservice/messagequeue/KafkaProducerConfig.java
@@ -19,7 +19,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
